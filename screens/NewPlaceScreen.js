4c3214112bc18a8c2caac545a8b995efc8d1d68a
richardwjy <49726866+richardwjy@users.noreply.github.com>
2021-05-12 21:41:41 +0700

    setup place navigation

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const NewPlaceScreen = props => {
+    return (
+        <View>
+            <Text>
+                NewPlaceScreen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default NewPlaceScreen;
---
+++ screens/PlacesListScreen.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const PlacesListScreen = props => {
+    return (
+        <View>
+            <Text>
+                PlacesListScreen
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default PlacesListScreen;
